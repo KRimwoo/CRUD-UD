812770d6ced6698ea89d444144a5cd1f4d0856a8
Woorim Kim <woorikim@c1r3s3.42seoul.kr>
2023-04-05 16:43:17 +0900

    add category, image field

--- session/blog/models.py
+++ session/blog/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 # Create your models here.
-# id(pk) 자동으로 생성됨
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     content = models.TextField()
+    image = models.ImageField(upload_to="post_image/", blank=True)
     
     def __str__(self):
         return self.title
--- session/blog/views.py
+++ session/blog/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from .models import Blog
+from .models import Blog, Category
 from .forms import BlogForm
 
 #render =>  response객체를 해당 url로 보내줌
 def home(request):
-    blogs = Blog.objects.all()
-    return render(request,'home.html',{'blogs':blogs})
+    category_id = request.GET.get('category')
+    if category_id:
+        blogs = Blog.objects.filter(category__id=category_id)
+    else:
+        blogs = Blog.objects.all()
+    categories = Category.objects.all()
+    return render(request,'home.html',{'blogs':blogs, 'categories':categories})
 
 def detail(request,blog_id):
     blog = get_object_or_404(Blog,pk=blog_id)
     return render(request,'detail.html',{'blog':blog})
 
 def new(request):
-    return render(request,'new.html')
-'''
-def create(request):
-    new_blog = Blog()
-    new_blog.title = request.POST['title'] #post->딕셔너리 값으로 바꿔줌
-    new_blog.content = request.POST['content']
-    new_blog.save()
-    return redirect('detail', new_blog.id)
-    #return render(request, 'detail.html', {'blog':new_blog}) <-새로고침하면 계속 post를 요청
-'''
+    categories = Category.objects.all() # 카테고리 정보를 가져옴
+    return render(request,'new.html', {'categories': categories})
+    
+#def create(request):
+#    new_blog = Blog()
+#    new_blog.title = request.POST['title'] #post->딕셔너리 값으로 바꿔줌
+#    new_blog.content = request.POST['content']
+#    new_blog.save()
+#    return redirect('detail', new_blog.id)
+#    #return render(request, 'detail.html', {'blog':new_blog}) <-새로고침하면 계속 post를 요청
+
 
 def create(request):
     form = BlogForm(request.POST)#post로 넘어온 입력값
@@ -35,16 +41,17 @@ def create(request):
 
 def edit(request, blog_id):
     edit_blog = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'edit.html', {'edit_blog':edit_blog})
+    categories = Category.objects.all()
+    return render(request, 'edit.html', {'edit_blog':edit_blog, 'categories': categories})
 
-'''
-def update(request, blog_id):
-    old_blog = get_object_or_404(Blog, pk=blog_id)
-    old_blog.title = request.POST['title']
-    old_blog.content = request.POST['content']
-    old_blog.save()
-    return redirect('detail', old_blog.id)
-'''
+
+#def update(request, blog_id):
+#    old_blog = get_object_or_404(Blog, pk=blog_id)
+#    old_blog.title = request.POST['title']
+#    old_blog.content = request.POST['content']
+#    old_blog.save()
+#    return redirect('detail', old_blog.id)
+
 def update(request, blog_id):
     old_blog = get_object_or_404(Blog, pk=blog_id)
     form = BlogForm(request.POST, instance=old_blog)
